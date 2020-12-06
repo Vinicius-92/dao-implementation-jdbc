a856173a3b855296cdc16f88b040bc22575390df
Vinicius Augusto <vinicius_augusto_@msn.com>
2020-12-06 18:02:09 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);	
+		Seller seller = new Seller(21, "Michael", "scott@dundlermifflin.com", new Date(), 4269.0, obj);
+		System.out.println(seller);	
 	}
 }
