4ec9a2c04264e7bcccef7dee6ded3221816ad56b
Vinicius Augusto <vinicius_augusto_@msn.com>
2020-12-06 19:13:53 -0300

    findById implementation and fixing previous type error

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,17 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(21, "Michael", "scott@dundlermifflin.com", new Date(), 4269.0, obj);
-		System.out.println(seller);	
-		
 		SellerDao sellerDao = DaoFactory.creatSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 	}
 }
